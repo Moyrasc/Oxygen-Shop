d763c81c2b4339bb3144afa083b5471bdf83fc12
Moyrasc <noemisolac@gmail.com>
2023-01-23 18:33:48 +0100

    working on form validation and submission

--- Js/main.js
+++ Js/main.js
@@ -39,3 +39,47 @@ buttonTop.addEventListener('click',()=>{
     })
 }, 200)
 })
+// validation form
+
+const form = document.querySelector('.form');
+const inputs = document.querySelector('.input_text');
+const nameForm = document.querySelector('.name');
+const emailForm= document.querySelector('.email')
+const errorName =document.querySelector('error_message_name');
+const errorEmail = document.querySelector('.error_message_email');
+const checkbox = document.querySelector('.checkbox')
+const policy = document.querySelector('.policy_label')
+
+const validateForm = (e) => {
+    const regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    const regexName = /[a-zA-Z]{2,100}/
+
+    if(e.target.name ==="name"){
+        if(regexName.test(e.target.value)){
+            errorName.style.visibility = "hidden";
+            nameForm.classList.remove('error')
+            nameForm.classList.add("correct")
+        }else {
+            errorName.style.visibility = "visible";
+            nameForm.classList.remove('error')
+            nameForm.classList.add("correct")
+        }
+    }
+    if(e.target.name === "email") {
+        if(regexMail.test(e.target.value)){
+            errorEmail.style.visibility = "hidden";
+            emailForm.classList.remove('error')
+            emailForm.classList.add("correct")
+        }else {
+            errorEmail.style.visibility = "visible";
+            emailForm.classList.remove('error')
+            emailForm.classList.add("correct")
+        }
+    }
+
+    inputs.forEach((input) => {
+    input.addEventListener("keydown", validateForm);
+});
+
+}
