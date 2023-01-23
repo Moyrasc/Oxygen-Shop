4ce975cb0c7d4b7e413cf0dba0a12dd5896366e6
Moyrasc <noemisolac@gmail.com>
2023-01-23 11:34:45 +0100

    add new branch

---
+++ Js/main.js
@@ -0,0 +1,5 @@
+//header-navbar
+const headerButtonBurguer = document.querySelector('.header_btn')
+const headerButtonClose = document.querySelector('.header_btn_close')
+const headerIcons = document.querySelectorAll('.header-icon')
+const navLinks = document.querySelector('./nav_links')
