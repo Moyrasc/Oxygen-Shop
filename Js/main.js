99dce41aa0fbae31e5b5cc079389b8addd862d5f
Moyrasc <noemisolac@gmail.com>
2023-01-23 13:04:35 +0100

    menu button with functionality

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,15 @@
 //header-navbar
 const headerButtonBurguer = document.querySelector('.header_btn')
 const headerButtonClose = document.querySelector('.header_btn_close')
-const headerIcons = document.querySelectorAll('.header-icon')
-const navLinks = document.querySelector('./nav_links')
+const headerIcons = document.querySelectorAll('.header_icon')
+const navLinks = document.querySelector('.nav_links')
+
+headerIcons.forEach(item => {
+    item.addEventListener("click",(e)=>{
+        navLinks.classList.toggle("nav_links_hidden");
+        headerButtonBurguer.classList.toggle("hidden");
+        headerButtonClose.classList.toggle("hidden");
+        
+    })
+})
+
